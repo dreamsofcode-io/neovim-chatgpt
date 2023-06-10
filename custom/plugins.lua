3ef053d3ac34067549e3d9f0f8d380ba2c1843c2
Elliott Minns <elliott.minns@pm.me>
2023-06-10 12:30:04 -0500

    initial commit

---
+++ custom/plugins.lua
@@ -0,0 +1,17 @@
+local plugins = {
+  {
+    "dreamsofcode-io/ChatGPT.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    },
+    config = function()
+      require("chatgpt").setup({
+        async_api_key_cmd = "pass show api/openai",
+      })
+    end,
+  }
+}
+return plugins
